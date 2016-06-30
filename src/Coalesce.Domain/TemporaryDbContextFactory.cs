4e840e12785f542190c1d1be9f98d38650aa015f
Dan Haley <dan.haley@IntelliTect.com>
2016-06-30 16:03:26 -0700

    Minor updates to get migrations to work

---
+++ src/Coalesce.Domain/TemporaryDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coalesce.Domain
+{
+    public class TemporaryDbContextFactory : IDbContextFactory<AppDbContext>
+    {
+        public AppDbContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CoalesceDb;Trusted_Connection=True;");
+            return new AppDbContext(builder.Options);
+        }
+    }
+}

53d0f0c7cf5e846ac56964dcdd8042048f8a5390
Andrew Scott <andrew.scott@intellitect.com>
2016-10-12 14:49:36 -0700

    Prepend property-name-based css classes with 'prop-' so that fields named 'hidden' aren't hidden.

--- src/IntelliTect.Coalesce/Helpers/Display.cs
+++ src/IntelliTect.Coalesce/Helpers/Display.cs
@@ -164,7 +164,7 @@ namespace IntelliTect.Coalesce.Helpers
         public static string PropertyHelperWithSurroundingDiv(PropertyViewModel prop, bool editable, string areaName = "")
         {
             var propertyHelper = PropertyHelper(prop, editable, areaName);
-            return $"<div class=\"col-md-8 {prop.JsonName}\">{propertyHelper}</div>";
+            return $"<div class=\"col-md-8 prop-{prop.JsonName}\">{propertyHelper}</div>";
         }
     }
 }

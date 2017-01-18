b4724a660d69ce3d06e7d01c78d201505306339d
Andrew Scott <andrew.scott@intellitect.com>
2017-01-18 13:23:30 -0800

    Implemented something of a solution to the partial classes problem.

---
+++ src/IntelliTect.Coalesce/DataAnnotations/TypeScriptPartialAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntelliTect.Coalesce.DataAnnotations
+{
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class TypeScriptPartialAttribute : Attribute
+    {
+        public string BaseClassName { get; set; }
+    }
+}

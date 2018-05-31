f9652fb429d2fc91fd6dfb696771cc0388eb5e96
Kelly Adams <kelly@intellitect.com>
2018-05-31 15:42:57 -0700

    You're killing me, Appveyor

--- build/before_build.ps1
+++ build/before_build.ps1
@@ -1,42 +1,39 @@
-Set-PSDebug -Trace 1
 Install-Product node 9
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
+if ($env:NPM_TOKEN -ne $null) {
   "//registry.npmjs.org/:_authToken=$env:NPM_TOKEN`n" | out-file "$env:userprofile\.npmrc" -Encoding ASCII
   npm whoami
-  if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 }
-node --version 
+Write-Host "Node version:"
+node --version
+Write-Host "Npm Version:"
 npm --version
 dotnet restore
 pushd .\src\coalesce-vue
 yarn
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 if ($env:APPVEYOR_BUILD_VERSION -ne $null) {
   yarn version --no-git-tag-version --new-version $env:APPVEYOR_BUILD_VERSION
-  if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 }
 yarn build
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
 popd
 pushd .\src\Coalesce.Web
 yarn
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
 popd
 pushd .\src\Coalesce.Web.Vue
 yarn
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 yarn global add gulp-cli
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
 <#
 Currently, our coalesce scripts for vue is in the knockout web project's gulpfile. 
 I don't want to add gulp to Coalesce.Web.Vue
 #>
+
 popd
 pushd .\src\Coalesce.Web
 gulp coalesce-ko
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
 popd
 pushd .\src\Coalesce.Web
 gulp coalesce-vue
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
       

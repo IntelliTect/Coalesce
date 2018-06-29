6826916b7df8bdc9ae2fc8cc0dec54c3a44b73b9
Kelly Adams <kelly@intellitect.com>
2018-06-29 14:17:08 -0700

    Putting scripts back in so %CONFIGURATION% envvar is set correctly.

--- build/init_master.ps1
+++
@@ -1,4 +0,0 @@
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
-  $env:coalesce_version = "$env:coalesce_version-release-pr-$env:APPVEYOR_PULL_REQUEST_NUMBER-$env:appveyor_build_number"
-}
-Update-AppveyorBuild -Version "$env:coalesce_version"

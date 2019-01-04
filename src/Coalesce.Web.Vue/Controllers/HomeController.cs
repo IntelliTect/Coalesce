046ef8527fcf3c4b9ce934dcd243309738bc80b2
Kelly Adams <kelly@intellitect.com>
2019-01-04 12:41:17 -0800

    Not hardcode the wwwroot convention

--- src/Coalesce.Web.Vue/Controllers/HomeController.cs
+++ src/Coalesce.Web.Vue/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 using System.Diagnostics;
-using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 
@@ -7,10 +7,17 @@ namespace Coalesce.Web.Vue.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
 
-            IFileProvider provider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"wwwroot"));
+            IFileProvider provider = new PhysicalFileProvider(hostingEnvironment.WebRootPath);
             IFileInfo fileInfo = provider.GetFileInfo("index.html");
             var readStream = fileInfo.CreateReadStream();
 

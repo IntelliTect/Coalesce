eb5e93d652bfbc597609f406046e09c40c27a71a
Andrew Scott <andrew.scott@intellitect.com>
2019-03-28 10:02:38 -0700

    Create our own mapping for CSV files so that CsvHelper doesn't stackoverflow when it finds a recursive reference

--- src/IntelliTect.Coalesce/Helpers/CsvHelper.cs
+++ src/IntelliTect.Coalesce/Helpers/CsvHelper.cs
@@ -13,48 +13,45 @@ namespace IntelliTect.Coalesce.Helpers
     /// </summary>
     public static class CsvHelper
     {
-        private static void Configure<T>(ClassMap map)
+        /// <summary>
+        /// CsvHelper's ClassMap class is abstract for no good reason.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class GenericMap<T> : ClassMap<T> { }
+
+        private static ClassMap CreateMap<T>()
         {
-            map.ReferenceMaps.Clear();
+            var vm = ReflectionRepository.Global.GetClassViewModel<T>();
 
-            //http://stackoverflow.com/questions/1571022/how-would-i-know-if-a-property-is-a-generic-collection
-            var membersToRemove = map.MemberMaps
-                .Where(p => {
-                    // Remove non-properties. Coalesce only exposes properties.
-                    if (p.Data.Member.MemberType != System.Reflection.MemberTypes.Property) return true;
+            var map = new GenericMap<T>();
 
-                    var memberType = new ReflectionTypeViewModel(p.Data.Member.MemberType());
-                    return memberType.IsA(typeof(ICollection<>));
-                })
-                .ToArray();
-
-            foreach (var pm in membersToRemove)
+            foreach (var prop in vm.ClientProperties)
             {
-                map.MemberMaps.Remove(pm);
+                if (!prop.Type.IsCollection && !prop.Type.IsPOCO)
+                {
+                    map.Map(typeof(T), prop.PropertyInfo);
+                }
             }
+
+            return map;
         }
 
         public static string CreateCsv<T>(IEnumerable<T> list)
         {
             var textWriter = new StringWriter();
             var csv = new CsvWriter(textWriter);
-            var map = csv.Configuration.AutoMap<T>();
-            Configure<T>(map);
-            csv.Configuration.RegisterClassMap(map);
+            csv.Configuration.RegisterClassMap(CreateMap<T>());
             csv.WriteRecords(list);
-            return textWriter.ToString();         
+            return textWriter.ToString();
         }
 
         public static IEnumerable<T> ReadCsv<T>(string text, bool hasHeader)
         {
             var textReader = new StringReader(text);
             var csv = new CsvReader(textReader);
-            var map = csv.Configuration.AutoMap<T>();
-            Configure<T>(map);
-            csv.Configuration.RegisterClassMap(map);
+            csv.Configuration.RegisterClassMap(CreateMap<T>());
             csv.Configuration.HasHeaderRecord = hasHeader;
-            var records = csv.GetRecords<T>().ToList();
-            return records;
+            return csv.GetRecords<T>().ToList();
         }
     }
 }

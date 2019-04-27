65db2961bbad59b1b2e5eb54d0fabcd0558a23b1
Andrew Scott <andrew.scott@intellitect.com>
2019-04-27 12:36:55 -0700

    Switching computers

--- src/coalesce-vue/src/viewmodel.ts
+++ src/coalesce-vue/src/viewmodel.ts
@@ -15,30 +15,76 @@ import * as apiClient from './api-client';
 import * as axios from 'axios';
 
 
-// /**
-//  * Dynamically adds gettter/setter properties to a class. These properties wrap the properties in its instances' $data objects.
-//  * @param ctor The class to add wrapper properties to
-//  * @param metadata The metadata describing the properties to add.
-//  */
-// export function defineProps<T extends new() => ViewModel<any, any>>(ctor: T, metadata: ModelType)
-// {
-//     Object.defineProperties(ctor.prototype,     
-//     Object
-//         .keys(metadata.props)
-//         .reduce(function (descriptors, propName) {
-//             descriptors[propName] = {
-//                 enumerable: true,
-//                 get: function(this: InstanceType<T>) {
-//                     return this.$data[propName]
-//                 },
-//                 set: function(this: InstanceType<T>, val: any) {
-//                     this.$data[propName] = val
-//                 }
-//             }
-//             return descriptors
-//         }, {} as PropertyDescriptorMap)
-//     )
-// }
+/**
+ * Dynamically adds gettter/setter properties to a class. These properties wrap the properties in its instances' $data objects.
+ * @param ctor The class to add wrapper properties to
+ * @param metadata The metadata describing the properties to add.
+ */
+export function defineProps<T extends new() => ViewModel<any, any>>(ctor: T, metadata: ModelType)
+{
+    Object.defineProperties(ctor.prototype,     
+    Object
+        .values(metadata.props)
+        .reduce(function (descriptors, prop) {
+            const propName = prop.name;
+            let value: any = null;
+            descriptors[propName] = {
+                enumerable: true,
+                get: function(this: InstanceType<T>) {
+                    return value
+                },
+                set: 
+                    prop.type == "model" ?
+                        function(this: InstanceType<T>, val: any) {
+                            if (val != null) {
+                                if (typeof val !== 'object') {
+                                    throw `Cannot assign a non-object to ${metadata.name}.${prop.name}`;
+                                }
+                                else if ('$metadata' in val) {
+                                    const incomingTypeMeta = (val as Model).$metadata;
+
+                                    if (incomingTypeMeta.name != prop.typeDef.name) {
+                                        throw `Type mismatch - attempted to assign a ${incomingTypeMeta.name} to ${metadata.name}.${prop.name}`;
+                                    }
+                                    
+                                    if (val instanceof ViewModel) {
+                                        // Already a viewmodel. Do nothing
+                                    }
+                                    else {
+                                        // Incoming is a Model. Make a ViewModel from it.
+                                        // This won't technically be valid according to the types of the properties
+                                        // on our generated ViewModels, but we should handle it
+                                        // so that input components work really nicely if a component sets the navigation prop.
+                                        if (ViewModel.typeLookup === null) {
+                                            throw "Static `ViewModel.typeLookup` is not defined. It should get defined in viewmodels.g.ts.";
+                                        }
+                                        var vmCtor = ViewModel.typeLookup[incomingTypeMeta.name].viewModel;
+                                        val = new vmCtor(val);
+                                    }
+                                }
+
+                                if (prop.role == "referenceNavigation") {
+                                    // When setting an object, fix up the foreign key using the value pulled from the object
+                                    // if it has a value.
+                                    const incomingPk = val[prop.principalKey.name];
+                                    if (incomingPk) {
+                                        (this as Indexable<InstanceType<T>>)[prop.foreignKey.name] = incomingPk;
+                                    }
+                                }
+                            }
+                            
+                            value = val;
+                        }
+                        // TODO: What do we do about collections?
+                    : function(this: InstanceType<T>, val: any) {
+                        this.$emit('valueChanged', prop, value, val);
+                        value = val;
+                    }
+            }
+            return descriptors
+        }, {} as PropertyDescriptorMap)
+    )
+}
 
 /*
 DESIGN NOTES
@@ -67,6 +113,9 @@ export abstract class ViewModel<
     TApi extends ModelApiClient<TModel> = ModelApiClient<TModel>,
     TPrimaryKey extends string | number = string | number
 > implements Model<TModel["$metadata"]> {
+    /** Static lookup of all generated ViewModel types. */
+    public static typeLookup: ViewModelTypeLookup | null = null;
+
     /**
      * Object which holds all of the data represented by this ViewModel.
      */
@@ -451,8 +500,8 @@ export type ViewModelFactory = (meta: ModelType) => ViewModel;
  * @param source The model whose values will be used to perform the update.
  */
 export function updateFromModel<TMeta extends ModelType, TModel extends Model<TMeta>>(
-    target: Model | ViewModel<TModel>, 
-    source: TModel,
+    target: Indexable<Model | ViewModel<TModel>>, 
+    source: Indexable<TModel>,
     viewModelFactory: ViewModelFactory
 ) {
 
@@ -461,11 +510,49 @@ export function updateFromModel<TMeta extends ModelType, TModel extends Model<TM
 
     for (const prop of Object.values(metadata.props)) {
         const propName = prop.name;
-        const sourceValue = (source as Indexable<TModel>)[propName];
-        const currentValue = (target as Indexable<TModel>)[propName];
+        const sourceValue = source[propName];
+        const currentValue = target[propName];
 
         switch (prop.role) {
             case "referenceNavigation":
+                if (sourceValue) {
+                    let viewModel: ViewModel;
+                    if (currentValue == null) {
+                        target[propName] = viewModel = viewModelFactory(prop.typeDef);
+                    } else {
+                        viewModel = currentValue;
+                    }
+
+                    viewModel.$loadFromModel(sourceValue);
+
+                    // Set the foreign key using the PK of the incoming object.
+                    // This may end up being redundant when our loop reaches the FK
+                    // property, but it will help ensure 100% correctness.
+                    target[prop.foreignKey.name] =
+                    source[prop.principalKey.name];
+                } else {
+                    // We allow the existing value of the navigation prop to stick around.
+                    // However, 
+                }
+                break;
+            case "foreignKey":
+                if (prop.navigationProp) {
+                    /*
+                        If there's a navigation property for this FK,
+                        we need to null it out if the following are true:
+                         - The current value of the navigation prop is non-null
+                         - The incoming value of the navigation prop IS null
+                         - The incoming value of the FK does not agree with
+                             the current PK on the current navigation prop.
+                        
+                        This allows us to keep the existing navigation object 
+                        even if the server didn't respond with one.
+                        However, if the FK has changed to a different value that no longer 
+                        agrees with the existing navigation object,
+                        we should clear it out to prevent an inconsistent data model.
+                    */
+                }
+                const sourceObject = source[prop.navigationProp]
             case "collectionNavigation":
 
         }

IntelliTect/Coalesce

commit aa2bfa5137aa84469841afe0ff72df0068b1b6a4

Property security annotations (#125)
* Implemented support for Create/Edit/Delete annotations for properties to improve support for the [File] annotation.

* Corrected bug where [Authorize] annotations were not being returned for properties. Added unit tests showing the PropertySecurityInfo attributes result in correct attributes and roles being generated on the controller.

* Refactored SecurityPropertyTests to reduce duplicated code.

* Refactored SecurityPropertyTests to be reusable.

* Incorporated feedback from code review:
Converted unit tests to XUnit,
Removed hard coded reference path

* Incorporated feedback from code review:
Rewording documentation,
Moved property initialization into SecurityInfoProperty.cs from PropertyViewModel.cs

* Refactored generation of the controller attributes from the SecurityInfoProperty into the ModelApiController in the generator.

* Corrected build error

src/IntelliTect.Coalesce.CodeGeneration.Api/Generators/ModelApiController.cs
@@ -223,10 +223,10 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
             {
                 var returnType = $"Task<ItemResult<{Model.DtoName}>>";
 
-                if (securityInfo.IsReadAllowed())
+                if (fileProperty.SecurityInfo.IsRead)
                 {
                     b.DocComment($"File Download: {fileProperty.Name}");
-                    b.Line($"{securityInfo.ReadAnnotation}");
+                    b.Line($"{ReadAnnotation(fileProperty.SecurityInfo)}");
                     b.Line($"[HttpGet(\"{fileProperty.FileControllerMethodName}\")]");
                     using (b.Block($"{Model.ApiActionAccessModifier} virtual async Task<IActionResult> {fileProperty.FileControllerMethodName}Get ({primaryKeyParameter}, {dataSourceParameter})"))
                     {
@@ -253,7 +253,7 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
                 if (securityInfo.IsCreateAllowed() || securityInfo.IsEditAllowed())
                 {
                     b.DocComment($"File Upload: {fileProperty.Name}");
-                    b.Line($"{securityInfo.SaveAnnotation}");
+                    b.Line($"{EditAnnotation(fileProperty.SecurityInfo)}");
                     b.Line($"[HttpPut(\"{fileProperty.FileControllerMethodName}\")]");
                     using (b.Block($"{Model.ApiActionAccessModifier} virtual async {returnType} {fileProperty.FileControllerMethodName}Put ({primaryKeyParameter}, IFormFile file, {dataSourceParameter}, IBehaviors<{Model.FullyQualifiedName}> behaviors)"))
                     {
@@ -293,7 +293,7 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
                     }
 
                     b.DocComment($"File Delete: {fileProperty.Name}");
-                    b.Line($"{securityInfo.SaveAnnotation}");
+                    b.Line($"{securityInfo.DeleteAnnotation}");
                     b.Line($"[HttpDelete (\"{fileProperty.FileControllerMethodName}\")]");
                     using (b.Block($"{Model.ApiActionAccessModifier} virtual async Task<IActionResult> {fileProperty.FileControllerMethodName}Delete ({primaryKeyParameter}, {dataSourceParameter}, IBehaviors<{Model.FullyQualifiedName}> behaviors)"))
                     {
@@ -320,5 +320,60 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
                 }
             }
         }
+
+
+
+        private bool AllowAnonymousAll(PropertySecurityInfo securityInfo)
+        {
+            return securityInfo.Read.AllowAnonymous &&
+                securityInfo.Edit.AllowAnonymous &&
+                securityInfo.Delete.AllowAnonymous;
+        }
+
+        /// <summary>
+        /// Returns an annotation for reading things (List/Get)
+        /// </summary>
+        private string ReadAnnotation(PropertySecurityInfo securityInfo)
+        {
+            if (securityInfo.Read.NoAccess) throw NoAccessException();
+            if (AllowAnonymousAll(securityInfo)) return string.Empty;
+            if (securityInfo.Read.AllowAnonymous || securityInfo.Edit.AllowAnonymous) return "[AllowAnonymous]";
+            if (securityInfo.Read.HasRoles) return $"[Authorize(Roles=\"{AllRoles(securityInfo)}\")]";
+
+            return "[Authorize]";
+        }
+
+        /// <summary>
+        /// Returns an annotation for editing things
+        /// </summary>
+        private string EditAnnotation(PropertySecurityInfo securityInfo)
+        {
+            
+            if (securityInfo.Edit.NoAccess) throw NoAccessException();
+            if (AllowAnonymousAll(securityInfo)) return string.Empty;
+            if (securityInfo.Edit.AllowAnonymous) return "[AllowAnonymous]";
+            if (securityInfo.Edit.HasRoles) return $"[Authorize(Roles=\"{securityInfo.Edit.ExternalRoleList}\")]";
+
+            return "[Authorize]";
+        }
+
+        private string AllRoles(PropertySecurityInfo securityInfo)
+        {
+
+            var result = securityInfo.Read.RoleList
+                .Union(securityInfo.Delete.RoleList)
+                .ToList();
+
+            if (result.Count() == 0) return "";
+            return string.Join(",", result);
+        }
+
+        /// <summary>
+        /// This is checked, and this exception thrown, to prevent accidents in code generation.
+        /// </summary>
+        /// <returns></returns>
+        private Exception NoAccessException()
+            => new InvalidOperationException(
+                $"Cannot emit an annotation for permission level {SecurityPermissionLevels.DenyAll}. Templates shouldn't emit anything in such cases.");
     }
 }

src/IntelliTect.Coalesce/DataAnnotations/SecurityInfoProperty.cs
@@ -1,4 +1,5 @@
-﻿using IntelliTect.Coalesce.Utilities;
+﻿using IntelliTect.Coalesce.Helpers;
+using IntelliTect.Coalesce.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,27 @@ namespace IntelliTect.Coalesce.DataAnnotations
     /// </summary>
     public class PropertySecurityInfo
     {
+        public PropertySecurityInfo(SecurityPermission read, SecurityPermission edit,
+            SecurityPermission delete)
+        {
+            Read = read;
+            Edit = edit;
+            Delete = delete;
+
+            IsRead = !read.NoAccess;
+            ReadRoles = read.Roles;
+
+            IsEdit = !edit.NoAccess;
+            EditRoles = edit.Roles;
+        }
         public bool IsEdit { get; set; } = false;
         public bool IsRead { get; set; } = false;
         public string EditRoles { get; set; } = "";
         public string ReadRoles { get; set; } = "";
 
-
+        public SecurityPermission Read { get; set; }
+        public SecurityPermission Edit { get; set; }
+        public SecurityPermission Delete { get; set; }
 
         public List<string> EditRolesList
         {
@@ -80,7 +96,7 @@ namespace IntelliTect.Coalesce.DataAnnotations
             }
             return true;
         }
-        
+
         // If true, the user can view the property
         public bool IsReadable(ClaimsPrincipal user)
         {
@@ -94,7 +110,7 @@ namespace IntelliTect.Coalesce.DataAnnotations
                         return false;
                     }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     // This happens when the trust is lost between the client and the domain.
                     if (ex.Message.Contains("trust")) return false;
@@ -103,6 +119,7 @@ namespace IntelliTect.Coalesce.DataAnnotations
             }
             return true;
         }
+
 
         public string ReadRolesExternal => string.Join(",", ReadRolesList);
 

src/IntelliTect.Coalesce/TypeDefinition/PropertyViewModel.cs
@@ -608,19 +608,10 @@ namespace IntelliTect.Coalesce.TypeDefinition
         {
             get
             {
-                var result = new PropertySecurityInfo();
-
-                if (HasAttribute<ReadAttribute>())
-                {
-                    result.IsRead = true;
-                    result.ReadRoles = this.GetAttributeValue<ReadAttribute>(a => a.Roles);
-                }
-
-                if (HasAttribute<EditAttribute>())
-                {
-                    result.IsEdit = true;
-                    result.EditRoles = this.GetAttributeValue<EditAttribute>(a => a.Roles);
-                }
+                var result = new PropertySecurityInfo(
+                    this.GetSecurityPermission<ReadAttribute>(),
+                    this.GetSecurityPermission<EditAttribute>(),
+                    this.GetSecurityPermission<DeleteAttribute>());
 
                 return result;
             }

Coalesce.sln
@@ -1,7 +1,7 @@
 ﻿
 Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio 15
-VisualStudioVersion = 15.0.27130.2036
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.28803.156
 MinimumVisualStudioVersion = 10.0.40219.1
 Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "src", "src", "{18165BA2-31FA-4D8E-94BE-AAD008D6BCEB}"
 EndProject
@@ -46,7 +46,9 @@ Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "build", "build", "{6AE7A58D
 		build\before_build.ps1 = build\before_build.ps1
 	EndProjectSection
 EndProject
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "IntelliTect.Coalesce.Swashbuckle", "src\IntelliTect.Coalesce.Swashbuckle\IntelliTect.Coalesce.Swashbuckle.csproj", "{7A0788D7-D705-476B-8D89-A0D49A1F201F}"
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "IntelliTect.Coalesce.Swashbuckle", "src\IntelliTect.Coalesce.Swashbuckle\IntelliTect.Coalesce.Swashbuckle.csproj", "{7A0788D7-D705-476B-8D89-A0D49A1F201F}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "IntelliTect.Coalesce.Web.Tests", "src\IntelliTect.Coalesce.Web.Tests\IntelliTect.Coalesce.Web.Tests.csproj", "{F3EB3AE9-0241-445B-8BED-3329E88D6E76}"
 EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
@@ -102,6 +104,10 @@ Global
 		{7A0788D7-D705-476B-8D89-A0D49A1F201F}.Debug|Any CPU.Build.0 = Debug|Any CPU
 		{7A0788D7-D705-476B-8D89-A0D49A1F201F}.Release|Any CPU.ActiveCfg = Release|Any CPU
 		{7A0788D7-D705-476B-8D89-A0D49A1F201F}.Release|Any CPU.Build.0 = Release|Any CPU
+		{F3EB3AE9-0241-445B-8BED-3329E88D6E76}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{F3EB3AE9-0241-445B-8BED-3329E88D6E76}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{F3EB3AE9-0241-445B-8BED-3329E88D6E76}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{F3EB3AE9-0241-445B-8BED-3329E88D6E76}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
@@ -119,6 +125,7 @@ Global
 		{C2259B45-4507-43C3-BA19-D3F07BE1C010} = {18165BA2-31FA-4D8E-94BE-AAD008D6BCEB}
 		{2F87C06D-22C5-453C-94DB-D78B1D8CAED6} = {18165BA2-31FA-4D8E-94BE-AAD008D6BCEB}
 		{7A0788D7-D705-476B-8D89-A0D49A1F201F} = {18165BA2-31FA-4D8E-94BE-AAD008D6BCEB}
+		{F3EB3AE9-0241-445B-8BED-3329E88D6E76} = {CCBE3CC2-7DA3-4BA1-96A1-7BAAF8533D58}
 	EndGlobalSection
 	GlobalSection(ExtensibilityGlobals) = postSolution
 		SolutionGuid = {00AED75F-9188-45D1-9D31-5486D4464AF2}

src/Coalesce.Domain/Case.cs
@@ -67,7 +67,10 @@ namespace Coalesce.Domain
 
         [File("image/jpeg", nameof(ImageName), nameof(ImageHash), nameof(ImageSize))]
         public byte[] Image { get; set; }
-        public string ImageName { get {
+        public string ImageName
+        {
+            get
+            {
                 return $"Case{CaseKey}.jpg";
             }
         }
@@ -78,8 +81,26 @@ namespace Coalesce.Domain
         public byte[] Attachment { get; set; }
         public string AttachmentName { get; set; }
 
+        [Edit(PermissionLevel = SecurityPermissionLevels.AllowAuthorized)]
+        [Read(PermissionLevel = SecurityPermissionLevels.AllowAll)]
         [File("text/plain")]
         public byte[] PlainAttachment { get; set; }
+
+        [Edit(PermissionLevel = SecurityPermissionLevels.AllowAuthorized, Roles = "Admin, SuperUser")]
+        [File]
+        public byte[] RestrictedUploadAttachment { get; set; }
+
+        [Read(PermissionLevel = SecurityPermissionLevels.AllowAuthorized, Roles = "Admin")]
+        [File]
+        public byte[] RestrictedDownloadAttachment { get; set; }
+
+        [File(NameProperty = nameof(InternalUseFileName),
+            SizeProperty = nameof(InternalUseFileSize))]
+        public byte[] RestrictedMetaAttachment { get; set; }
+        [InternalUse]
+        public string InternalUseFileName { get; set; }
+        [InternalUse]
+        public long InternalUseFileSize { get; set; }
 
         public string Severity { get; set; }
 

commit d115946c56188d72faa4f35cc4e084439bd7d495

Fix security behavior issues introduced by aa2bfa5. Also fixes #119.

src/IntelliTect.Coalesce.CodeGeneration.Api/Generators/ModelApiController.cs
@@ -265,12 +265,12 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
 
                             b.Line($"itemResult.Object.{fileProperty.Name} = stream.ToArray();");
 
-                            if (fileProperty.FileNameProperty?.HasSetter ?? false)
+                            if (fileProperty.FileNameProperty?.HasPublicSetter ?? false)
                             {
                                 b.Line($"itemResult.Object.{fileProperty.FileNameProperty.Name} = file.FileName;");
                             }
 
-                            if (fileProperty.FileHashProperty?.HasSetter ?? false)
+                            if (fileProperty.FileHashProperty?.HasPublicSetter ?? false)
                             {
                                 using (b.Block("using (var sha256Hash = System.Security.Cryptography.SHA256.Create())"))
                                 {
@@ -279,7 +279,7 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
                                 }
                             }
 
-                            if (fileProperty.FileSizeProperty?.HasSetter ?? false)
+                            if (fileProperty.FileSizeProperty?.HasPublicSetter ?? false)
                             {
                                 b.Line($"itemResult.Object.{fileProperty.FileSizeProperty.Name} = file.Length;");
                             }
@@ -300,15 +300,15 @@ namespace IntelliTect.Coalesce.CodeGeneration.Api.Generators
                         b.Line($"var (itemResult, _) = await dataSource.GetItemAsync(id, new ListParameters());");
                         b.Line($"if (!itemResult.WasSuccessful) return NotFound();");
 
-                        if (fileProperty.FileNameProperty?.HasSetter ?? false)
+                        if (fileProperty.FileNameProperty?.HasPublicSetter ?? false)
                         {
                             b.Line($"itemResult.Object.{fileProperty.FileNameProperty.Name} = null;");
                         }
-                        if (fileProperty.FileHashProperty?.HasSetter ?? false)
+                        if (fileProperty.FileHashProperty?.HasPublicSetter ?? false)
                         {
                             b.Line($"itemResult.Object.{fileProperty.FileHashProperty.Name} = null;");
                         }
-                        if (fileProperty.FileSizeProperty?.HasSetter ?? false)
+                        if (fileProperty.FileSizeProperty?.HasPublicSetter ?? false)
                         {
                             b.Line($"itemResult.Object.{fileProperty.FileSizeProperty.Name} = 0;");
                         }

src/IntelliTect.Coalesce.Tests/TargetClasses/PropSec.cs
@@ -1,0 +1,24 @@
+﻿using IntelliTect.Coalesce.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace IntelliTect.Coalesce.Tests.TargetClasses
+{
+    public class PropSec
+    {
+        [Read]
+        public string ReadOnlyViaRead { get; set; }
+
+        [ReadOnly(true)]
+        public string ReadOnlyViaReadOnly { get; set; }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        [ReadOnlyApi]
+#pragma warning restore CS0618 // Type or member is obsolete
+        public string ReadOnlyViaReadOnlyApi { get; set; }
+
+        public string ReadOnlyViaNonPublicSetter { get; internal set; }
+    }
+}

src/IntelliTect.Coalesce/DataAnnotations/SecurityInfoProperty.cs
@@ -23,8 +23,16 @@ namespace IntelliTect.Coalesce.DataAnnotations
             IsRead = !read.NoAccess;
             ReadRoles = read.Roles;
 
-            IsEdit = !edit.NoAccess;
-            EditRoles = edit.Roles;
+            if (read.HasAttribute && !edit.HasAttribute)
+            {
+                IsEdit = false;
+                EditRoles = "";
+            }
+            else
+            {
+                IsEdit = !edit.NoAccess;
+                EditRoles = edit.Roles;
+            }
         }
         public bool IsEdit { get; set; } = false;
         public bool IsRead { get; set; } = false;

src/IntelliTect.Coalesce/TypeDefinition/ClassViewModel.cs
@@ -181,7 +181,7 @@ namespace IntelliTect.Coalesce.TypeDefinition
 
         public IEnumerable<PropertyViewModel> DataSourceParameters => Properties
             .Where(p =>
-                !p.IsInternalUse && p.HasSetter && p.HasAttribute<CoalesceAttribute>()
+                !p.IsInternalUse && p.HasPublicSetter && p.HasAttribute<CoalesceAttribute>()
                 // These are the only supported types, for now
                 && (p.Type.IsPrimitive || p.Type.IsDate)
             );

src/IntelliTect.Coalesce/TypeDefinition/PropertyViewModel.cs
@@ -33,6 +33,8 @@ namespace IntelliTect.Coalesce.TypeDefinition
 
         public abstract bool HasSetter { get; }
 
+        public abstract bool HasPublicSetter { get; }
+
         public abstract bool IsVirtual { get; }
 
         public abstract bool IsStatic { get; }
@@ -153,7 +155,7 @@ namespace IntelliTect.Coalesce.TypeDefinition
         /// <para/>
         /// This includes normal, writable value properties, as well as primary keys.
         /// </summary>
-        public bool IsClientSerializable => (IsClientWritable || IsPrimaryKey) && HasSetter && !IsPOCO && !Type.IsCollection;
+        public bool IsClientSerializable => (IsClientWritable || IsPrimaryKey) && HasPublicSetter && !IsPOCO && !Type.IsCollection;
 
         /// <summary>
         /// True if the value of the property can be modified by the client in a persistable way.
@@ -166,7 +168,7 @@ namespace IntelliTect.Coalesce.TypeDefinition
         /// </summary>
         public bool IsClientWritable => 
             !IsInternalUse 
-            && HasSetter 
+            && HasPublicSetter
             // Exclude object properties with setters that aren't DB mapped - 
             // these are probably Owned Types, which we don't currently support editing.
             && (!IsPOCO || Object.HasDbSet) 

src/IntelliTect.Coalesce/TypeDefinition/Reflection/ReflectionPropertyViewModel.cs
@@ -27,6 +27,8 @@ namespace IntelliTect.Coalesce.TypeDefinition
 
         public override bool HasSetter => Info.CanWrite;
 
+        public override bool HasPublicSetter => HasSetter && Info.SetMethod.IsPublic;
+
         public override PropertyInfo PropertyInfo => Info;
         
         public override bool IsVirtual => Info.GetGetMethod()?.IsVirtual ?? Info.GetSetMethod()?.IsVirtual ?? false;

src/IntelliTect.Coalesce/TypeDefinition/Symbol/SymbolPropertyViewModel.cs
@@ -34,6 +34,8 @@ namespace IntelliTect.Coalesce.TypeDefinition
 
         public override bool HasSetter => !Symbol.IsReadOnly;
 
+        public override bool HasPublicSetter => HasSetter && Symbol.SetMethod.DeclaredAccessibility == Accessibility.Public;
+
         public override bool IsStatic => Symbol.IsStatic;
 
         public override bool IsVirtual => Symbol.IsVirtual;

src/IntelliTect.Coalesce.Tests/Tests/Security/PropertySecurityTests.cs
@@ -1,0 +1,28 @@
+﻿using IntelliTect.Coalesce.Tests.Util;
+using IntelliTect.Coalesce.TypeDefinition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using IntelliTect.Coalesce.Tests.TargetClasses;
+
+namespace IntelliTect.Coalesce.Tests.Tests.Security
+{
+    public class PropertySecurityTests
+    {
+        [Theory]
+        [ClassViewModelData(typeof(PropSec), nameof(PropSec.ReadOnlyViaRead))]
+        [ClassViewModelData(typeof(PropSec), nameof(PropSec.ReadOnlyViaReadOnly))]
+        [ClassViewModelData(typeof(PropSec), nameof(PropSec.ReadOnlyViaReadOnlyApi))]
+        [ClassViewModelData(typeof(PropSec), nameof(PropSec.ReadOnlyViaNonPublicSetter))]
+        public void ReadOnly_CorrectForReadOnlyProps(ClassViewModelData data, string propName)
+        {
+            ClassViewModel vm = data;
+            var prop = vm.PropertyByName(propName);
+
+            Assert.True(prop.IsReadOnly);
+            Assert.False(prop.IsClientSerializable);
+            Assert.False(prop.IsClientWritable);
+        }
+    }
+}
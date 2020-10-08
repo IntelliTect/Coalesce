519a530dbb77b0b75de70e917e4db13fa33b8aa8
Andrew Scott <ascott18@gmail.com>
2020-10-08 15:53:59 -0700

    Inline dependency on get-current-script so consuming projects aren't required to install it.

--- src/coalesce-vue-vuetify/vue.config.js
+++ src/coalesce-vue-vuetify/vue.config.js
@@ -1,20 +1,21 @@
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
-const nodeExternals = require('webpack-node-externals');
-
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  "lintOnSave": false,
-  
+  lintOnSave: false,
+
   configureWebpack: {
-    externals: [ nodeExternals() ]
+    externals: [nodeExternals({
+      allowlist: [
+        /get\-current\-script/
+      ]
+    })]
   },
 
   chainWebpack: config => {
-    config
-      .plugin('fork-ts-checker')
-      .tap(args => {
-        args[0].tsconfig = './tsconfig.build.json';
-        return args;
-      });
+    config.plugin("fork-ts-checker").tap(args => {
+      args[0].tsconfig = "./tsconfig.build.json";
+      return args;
+    });
   }
-}
+};

b8abdb0e30a38b13e9c3e03ec4caa281869f7c6b
Andrew Scott <ascott18@gmail.com>
2020-12-17 16:32:23 -0800

    Make the computed collection properties on ListParameters truly read only.
    This makes it so the ASP.NET Core model binder doesn't see them and think that it can incorrectly add items into the collections. Caching was also added to OrderByList as it was previously not present, causing the dictionary to be reconstructed at least 3 times per /list request.
    
    This lack of true read-onlyness was allowing requests with extra parameters that shouldn't have been getting read (FieldList) to end up actually modifying the cached field list collection.
    
    For example:
    http://localhost:5000/api/Person/list?Page=&PageSize=&OrderBy=&OrderByDescending=&Fields=name&FieldList=System.Collections.Generic.List`1[System.String]&OrderByList=System.Collections.Generic.Dictionary`2[System.String%2cIntelliTect.Coalesce.SortDirection]&Search=&Includes=&DataSource=
    
    These extra parameters might end up here if someone is directly serializing a ListParameters object to make an outgoing request from a C# application.

--- src/IntelliTect.Coalesce/Api/Parameters/ListParameters.cs
+++ src/IntelliTect.Coalesce/Api/Parameters/ListParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,32 +23,50 @@ namespace IntelliTect.Coalesce.Api
         /// <inheritdoc />
         public string? Fields { get; set; }
 
+        private string? _cachedFields;
         private IReadOnlyCollection<string>? _fieldList;
         /// <inheritdoc cref="IListParameters.Fields" />
         public IReadOnlyCollection<string> FieldList
         {
             get
             {
-                if (_fieldList != null)
+                if (_fieldList != null && Fields == _cachedFields)
                 {
                     return _fieldList;
                 }
+
+                // Threadsafety - null _fieldList so an intermediate state isn't incorrectly served.
+                _fieldList = null;
+                _cachedFields = Fields;
                 if (Fields == null)
                 {
-                    return _fieldList = new List<string>();
+                    return _fieldList = new List<string>()
+                        .AsReadOnly();
                 }
-                return _fieldList = Fields.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                return _fieldList = Fields
+                    .Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList()
+                    .AsReadOnly();
             }
         }
         
         /// <inheritdoc />
         IReadOnlyCollection<string> IListParameters.Fields => FieldList;
-        
+
         /// <inheritdoc cref="IListParameters.OrderByList" />
-        public Dictionary<string, SortDirection> OrderByList
+        private string? _cachedOrderBy;
+        private string? _cachedOrderByDescending;
+        private IReadOnlyDictionary<string, SortDirection>? _orderByList;
+        public IReadOnlyDictionary<string, SortDirection> OrderByList
         {
             get
             {
+                if (_orderByList != null && OrderBy == _cachedOrderBy && OrderByDescending == _cachedOrderByDescending)
+                {
+                    return _orderByList;
+                }
+
                 var result = new Dictionary<string, SortDirection>();
                 // Add order by
                 if (OrderBy != null)
@@ -85,7 +104,13 @@ namespace IntelliTect.Coalesce.Api
                         }
                     }
                 }
-                return result;
+
+
+                // Threadsafety - null _orderByList so an intermediate state isn't incorrectly served.
+                _orderByList = null;
+                _cachedOrderBy = OrderBy;
+                _cachedOrderByDescending = OrderByDescending;
+                return _orderByList = new ReadOnlyDictionary<string, SortDirection>(result);
             }
         }
         

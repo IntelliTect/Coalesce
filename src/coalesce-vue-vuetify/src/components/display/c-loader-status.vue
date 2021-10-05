01be9150ed15b86b5453e395a9d5a319c5fc1a79
Andrew Scott <ascott18@gmail.com>
2021-10-05 12:05:51 -0700

    Make `no-initial-content` `c-loader-status` flag also hide content when the loader has errored and also has no previous `result`.

--- src/coalesce-vue-vuetify/src/components/display/c-loader-status.vue
+++ src/coalesce-vue-vuetify/src/components/display/c-loader-status.vue
@@ -175,8 +175,13 @@ export default class extends Vue {
         // loader has an error, and error content is off
         return false;
       }
-      if (loader.wasSuccessful == null && !flags.initialContent) {
+      if (
         // loader has not yet loaded, and initial content is off
+        (loader.wasSuccessful == null && !flags.initialContent) || 
+        // or loader has loaded, but it errored and there's no current result
+        // (implying it has never successfully loaded).
+        (loader.wasSuccessful === false && !loader.hasResult)
+      ) {
         return false;
       }
       return true;

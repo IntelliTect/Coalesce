6fe95608e0e629fae10fbdabc846554919c66d28
Andrew Scott <ascott18@gmail.com>
2022-09-01 16:46:53 -0700

    Make CoalesceVuetifyResolver automatically detect if coalesce-vue-vuetify2 was aliased as coalesce-vue-vuetify via package.json

--- src/coalesce-vue-vuetify2/src/build.ts
+++ src/coalesce-vue-vuetify2/src/build.ts
@@ -1,11 +1,30 @@
 /** Component name resolver for unplugin-vue-components. */
 
+let moduleName: Promise<string>;
 export function CoalesceVuetifyResolver() {
+  moduleName = (async () => {
+    // See if coalesce-vue-vuetify2 was aliased in package.json as coalesce-vue-vuetify.
+    // We have to do so in a way that will work in both ESM and CJS.
+
+    if ("require" in global) {
+      try {
+        require.resolve("coalesce-vue-vuetify");
+        return "coalesce-vue-vuetify";
+      } catch {}
+    } else {
+      try {
+        if (await import.meta.resolve?.("coalesce-vue-vuetify"))
+          return "coalesce-vue-vuetify";
+      } catch {}
+    }
+    return "coalesce-vue-vuetify2";
+  })();
+
   return {
     type: "component",
-    resolve: (name: string) => {
+    resolve: async (name: string) => {
       if (name.match(/^C[A-Z]/))
-        return { name, from: "coalesce-vue-vuetify/lib" };
+        return { name, from: (await moduleName) + "/lib" };
     },
   } as const;
 }

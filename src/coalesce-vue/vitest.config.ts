28ce00972dcf1b6a34398bef079e0cdc5b632fde
Andrew Scott <ascott18@gmail.com>
2022-11-23 16:00:25 -0800

    Fix coalesce-vue tests

--- src/coalesce-vue/vitest.config.ts
+++ src/coalesce-vue/vitest.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig, UserConfig } from 'vitest/config'
+import { defineConfig, UserConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
   define: {
-    TEST_EXPECTED_VUE_VERSION: 2
+    TEST_EXPECTED_VUE_VERSION: 2,
   },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["test/global-setup.ts"],
-    include: ['**/*.{test,spec}.{ts,js}', '**/*.{test,spec}.vue2.{ts,js}']
+    include: ["**/*.{test,spec}.{ts,js}", "**/*.{test,spec}.vue2.{ts,js}"],
   },
-}) as UserConfig
+}) as UserConfig;

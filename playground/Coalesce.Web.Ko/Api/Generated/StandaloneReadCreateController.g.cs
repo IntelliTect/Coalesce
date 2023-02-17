622f3029dbdedca4850d03b8ef2b953a2397ef51
Andrew Scott <ascott18@gmail.com>
2023-02-17 14:55:16 -0800

    Fix multiple build issues: - https://github.com/mozilla/source-map/issues/349 - https://github.com/nodejs/node/issues/46542 https://github.com/npm/cli/pull/6142

---
+++ playground/Coalesce.Web.Ko/Api/Generated/StandaloneReadCreateController.g.cs
@@ -0,0 +1,73 @@
+
+using Coalesce.Web.Ko.Models;
+using IntelliTect.Coalesce;
+using IntelliTect.Coalesce.Api;
+using IntelliTect.Coalesce.Api.Controllers;
+using IntelliTect.Coalesce.Api.DataSources;
+using IntelliTect.Coalesce.Mapping;
+using IntelliTect.Coalesce.Mapping.IncludeTrees;
+using IntelliTect.Coalesce.Models;
+using IntelliTect.Coalesce.TypeDefinition;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Coalesce.Web.Ko.Api
+{
+    [Route("api/StandaloneReadCreate")]
+    [Authorize]
+    [ServiceFilter(typeof(IApiActionFilter))]
+    public partial class StandaloneReadCreateController
+        : BaseApiController<Coalesce.Domain.StandaloneReadCreate, StandaloneReadCreateDtoGen>
+    {
+        public StandaloneReadCreateController() : base()
+        {
+            GeneratedForClassViewModel = ReflectionRepository.Global.GetClassViewModel<Coalesce.Domain.StandaloneReadCreate>();
+        }
+
+        [HttpGet("get/{id}")]
+        [Authorize]
+        public virtual Task<ItemResult<StandaloneReadCreateDtoGen>> Get(
+            int id,
+            DataSourceParameters parameters,
+            IDataSource<Coalesce.Domain.StandaloneReadCreate> dataSource)
+            => GetImplementation(id, parameters, dataSource);
+
+        [HttpGet("list")]
+        [Authorize]
+        public virtual Task<ListResult<StandaloneReadCreateDtoGen>> List(
+            ListParameters parameters,
+            IDataSource<Coalesce.Domain.StandaloneReadCreate> dataSource)
+            => ListImplementation(parameters, dataSource);
+
+        [HttpGet("count")]
+        [Authorize]
+        public virtual Task<ItemResult<int>> Count(
+            FilterParameters parameters,
+            IDataSource<Coalesce.Domain.StandaloneReadCreate> dataSource)
+            => CountImplementation(parameters, dataSource);
+
+        [HttpPost("save")]
+        [Authorize]
+        public virtual Task<ItemResult<StandaloneReadCreateDtoGen>> Save(
+            [FromForm] StandaloneReadCreateDtoGen dto,
+            [FromQuery] DataSourceParameters parameters,
+            IDataSource<Coalesce.Domain.StandaloneReadCreate> dataSource,
+            IBehaviors<Coalesce.Domain.StandaloneReadCreate> behaviors)
+            => SaveImplementation(dto, parameters, dataSource, behaviors);
+
+        [HttpPost("delete/{id}")]
+        [Authorize]
+        public virtual Task<ItemResult<StandaloneReadCreateDtoGen>> Delete(
+            int id,
+            IBehaviors<Coalesce.Domain.StandaloneReadCreate> behaviors,
+            IDataSource<Coalesce.Domain.StandaloneReadCreate> dataSource)
+            => DeleteImplementation(id, new DataSourceParameters(), dataSource, behaviors);
+    }
+}
---
+++ playground/Coalesce.Web.Ko/Controllers/Generated/StandaloneReadCreateController.g.cs
@@ -0,0 +1,42 @@
+using IntelliTect.Coalesce.Knockout.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Coalesce.Web.Ko.Controllers
+{
+    [Authorize]
+    public partial class StandaloneReadCreateController : BaseViewController<Coalesce.Domain.StandaloneReadCreate>
+    {
+        [Authorize]
+        public ActionResult Cards()
+        {
+            return IndexImplementation(false, @"~/Views/Generated/StandaloneReadCreate/Cards.cshtml");
+        }
+
+        [Authorize]
+        public ActionResult Table()
+        {
+            return IndexImplementation(false, @"~/Views/Generated/StandaloneReadCreate/Table.cshtml");
+        }
+
+
+        [Authorize]
+        public ActionResult TableEdit()
+        {
+            return IndexImplementation(true, @"~/Views/Generated/StandaloneReadCreate/Table.cshtml");
+        }
+
+        [Authorize]
+        public ActionResult CreateEdit()
+        {
+            return CreateEditImplementation(@"~/Views/Generated/StandaloneReadCreate/CreateEdit.cshtml");
+        }
+
+        [Authorize]
+        public ActionResult EditorHtml(bool simple = false)
+        {
+            return EditorHtmlImplementation(simple);
+        }
+    }
+}

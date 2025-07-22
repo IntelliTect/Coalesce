97a1d406b503b2794226fca7d84bedf1a9d851dc
Andrew Scott <ascott18@gmail.com>
2025-07-22 11:59:24 -0700

    wip

---
+++ src/IntelliTect.Coalesce.Analyzer/Analyzers/ExampleAnalyzer.cs
@@ -0,0 +1,72 @@
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class InvalidSecurityAttributeUsage : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor _Rule = new(
+        id: "COALESCE0202",
+        title: "Invalid PermissionLevel usage on property security attributes",
+        messageFormat: "Property-level {0} attributes should not set PermissionLevel - use roles instead",
+        category: "Usage",
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        description: "PermissionLevel is only valid for class-level security attributes. For property-level security, use the Roles property to specify role-based access control.");
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        if (context is null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.EnableConcurrentExecution();
+        context.RegisterSymbolAction(AnalyzeProperty, SymbolKind.Property);
+    }
+
+    private static void AnalyzeProperty(SymbolAnalysisContext context)
+    {
+        var propertySymbol = (IPropertySymbol)context.Symbol;
+
+        foreach (var attributeData in propertySymbol.GetAttributes())
+        {
+            var attributeType = attributeData.AttributeClass;
+            var attributeTypeName = attributeType?.Name;
+            var attributeNamespace = attributeType?.ContainingNamespace?.ToDisplayString();
+
+            if (attributeNamespace != "IntelliTect.Coalesce.DataAnnotations")
+                continue;
+
+            if (attributeTypeName != "ReadAttribute" && attributeTypeName != "EditAttribute")
+                continue;
+
+            // Check if PermissionLevel is being set
+            if (HasPermissionLevelArgument(attributeData))
+            {
+                var attributeName = attributeTypeName.Replace("Attribute", "");
+                var diagnostic = Diagnostic.Create(_Rule, propertySymbol.Locations[0], attributeName);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+
+    private static bool HasPermissionLevelArgument(AttributeData attributeData)
+    {
+        // Check named arguments for PermissionLevel
+        foreach (var namedArg in attributeData.NamedArguments)
+        {
+            if (namedArg.Key == "PermissionLevel")
+                return true;
+        }
+
+        // Check constructor arguments - if any argument is SecurityPermissionLevels enum
+        foreach (var constructorArg in attributeData.ConstructorArguments)
+        {
+            if (constructorArg.Type?.Name == "SecurityPermissionLevels")
+                return true;
+        }
+
+        return false;
+    }
+}
--- src/IntelliTect.Coalesce/TypeDefinition/Helpers/IAttributeProvider.cs
+++ src/IntelliTect.Coalesce/TypeDefinition/Helpers/IAttributeProvider.cs
@@ -8,67 +8,6 @@ using System.Linq.Expressions;
 
 namespace IntelliTect.Coalesce.TypeDefinition;
 
-public abstract class AttributeViewModel<TAttribute>
-    where TAttribute : Attribute
-{
-    public ReflectionRepository ReflectionRepository { get; }
-
-    public abstract TypeViewModel Type { get; }
-
-    protected AttributeViewModel(ReflectionRepository? reflectionRepository)
-    {
-        ReflectionRepository = reflectionRepository ?? ReflectionRepository.Global;
-    }
-
-    public T? GetValue<T>(string valueName)
-        where T : struct
-    {
-        var result = GetValue(valueName);
-        if (result == null)
-        {
-            return null;
-        }
-        return new T?((T)result);
-    }
-
-    public string? GetValue(Expression<Func<TAttribute, string?>> propertyExpression)
-    {
-        return GetValue(propertyExpression.GetExpressedProperty().Name) as string;
-    }
-
-    public T? GetValue<T>(Expression<Func<TAttribute, T?>> propertyExpression)
-        where T : class
-    {
-        return GetValue(propertyExpression.GetExpressedProperty().Name) as T;
-    }
-
-    public T? GetValue<T>(
-        Expression<Func<TAttribute, T>> propertyExpression,
-        Expression<Func<TAttribute, bool>>? hasValueExpression = null
-    )
-        where T : struct
-    {
-        // In reflection contexts, for attributes that can have "unset" as a value that means null,
-        // we have to check this state with an additional property. E.g. ExecuteAttribute.ValidateAttributes.
-        // In symbol contexts, the absence of syntax that assigns a value to the property implies this "unset" state.
-        if (hasValueExpression is not null)
-        {
-            // `hasValue` will always be non-null in reflection contexts,
-            // and always null in symbol contexts where the HasValue property won't ever be set.
-            var hasValue = GetValue<bool>(hasValueExpression.GetExpressedProperty().Name);
-            if (hasValue == false)
-            {
-                return null;
-            }
-        }
-
-
-        return GetValue<T>(propertyExpression.GetExpressedProperty().Name);
-    }
-
-    public abstract object? GetValue(string valueName);
-}
-
 public interface IAttributeProvider
 {
     IEnumerable<AttributeViewModel<TAttribute>> GetAttributes<TAttribute>() where TAttribute : Attribute;
@@ -80,7 +19,7 @@ public static class AttributeExtensions
         where TAttribute : Attribute
         => obj.GetAttributes<TAttribute>();
 
-    public static AttributeViewModel<TAttribute>? GetAttribute<TAttribute>(this IAttributeProvider obj) 
+    public static AttributeViewModel<TAttribute>? GetAttribute<TAttribute>(this IAttributeProvider obj)
         where TAttribute : Attribute
         => obj.GetAttributes<TAttribute>().FirstOrDefault();
 
@@ -124,7 +63,7 @@ public static class AttributeExtensions
     }
 
     public static (bool Exists, T? Value, string? Message) GetValidationAttribute<TAttribute, T>(
-        this IAttributeProvider obj, 
+        this IAttributeProvider obj,
         Expression<Func<TAttribute, T>> propertyExpression
     )
         where TAttribute : ValidationAttribute

b2dfae52ef817890695fffcc0ba022d74cffcee7
Andrew Scott <ascott18@gmail.com>
2025-07-22 12:54:29 -0700

    fix issues

--- src/IntelliTect.Coalesce/Application/CoalesceApplicationBuilderExtensions.cs
+++ src/IntelliTect.Coalesce/Application/CoalesceApplicationBuilderExtensions.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Reflection;
 
 namespace IntelliTect.Coalesce;
 

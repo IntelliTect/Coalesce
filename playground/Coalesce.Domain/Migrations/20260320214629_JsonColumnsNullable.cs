cb2206889694194fc5375ab8d3b1f716e12704de
Andrew Scott <andrew.scott@intellitect.com>
2026-03-20 15:30:48 -0700

    fix: missing migration

---
+++ playground/Coalesce.Domain/Migrations/20260320214629_JsonColumnsNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coalesce.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class JsonColumnsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WeatherHistory",
+                table: "Person",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WeatherHistory",
+                table: "Person",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
